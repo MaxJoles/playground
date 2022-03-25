b3825f04dc8e1db37c96b3f530efd45861335ff0
Max <mpjoles@gmail.com>
2022-03-25 16:34:41 -0400

    auto

--- div-hunter.js
+++ div-hunter.js
@@ -3,20 +3,18 @@ const hunterOutput = document.getElementById("hunter-result");
 const hunterLink = document.getElementById("hunter-link");
 
 
-function divHunter(url) {
+function divHunter() {
     const styleSheets = document.styleSheets;
     for (const key in styleSheets) {
-        if (styleSheets.hasOwnProperty.call(styleSheets, key)) {
             let styleSheet = styleSheets[key];
+            console.log(styleSheet.href);
             let styeSheetUrl = new URL(styleSheet.href);
             let styleSheetDomain = styeSheetUrl.hostname;
-
             if (styleSheetDomain == "assets-global.website-files.com" || styleSheetDomain == "uploads-ssl.webflow.com") {
                 console.log("Is Webflow Sitemap")
                 console.log(styeSheetUrl);
                 console.log(styleSheet.cssRules)
             }
-        }
     }
 }
 
