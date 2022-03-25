987e58d1719dfe3aa1c5735ac7704c7130beb87e
Max <mpjoles@gmail.com>
2022-03-25 16:30:13 -0400

    auto

--- div-hunter.js
+++ div-hunter.js
@@ -1,29 +1,29 @@
 const hunterForm = document.getElementById("hunter-form");
 const hunterOutput = document.getElementById("hunter-result");
 const hunterLink = document.getElementById("hunter-link");
-const styleSheets = document.styleSheets;
 
 
-for (const key in styleSheets) {
-    if (styleSheets.hasOwnProperty.call(styleSheets, key)) {
-        let styleSheet = styleSheets[key];
-        let styeSheetUrl = new URL (styleSheet.href);
-        let styleSheetDomain = styeSheetUrl.hostname;
+function divHunter(url) {
+    const styleSheets = document.styleSheets;
+    for (const key in styleSheets) {
+        if (styleSheets.hasOwnProperty.call(styleSheets, key)) {
+            let styleSheet = styleSheets[key];
+            let styeSheetUrl = new URL(styleSheet.href);
+            let styleSheetDomain = styeSheetUrl.hostname;
 
-        if (styleSheetDomain == "assets-global.website-files.com" || styleSheetDomain == "uploads-ssl.webflow.com" ) {
-            console.log("Is Webflow Sitemap")
-            console.log(styeSheetUrl);
+            if (styleSheetDomain == "assets-global.website-files.com" || styleSheetDomain == "uploads-ssl.webflow.com") {
+                console.log("Is Webflow Sitemap")
+                console.log(styeSheetUrl);
+                console.log(styleSheet.cssRules)
+            }
         }
-
-
-        console.log(styleSheet);
-     
     }
 }
 
+
+
 hunterForm.addEventListener("submit", () => {
     let siteLink = hunterLink.value;
-    console.log(siteLink);
     console.log("Form Submitted");
+})
 
-} )
