0ff3f59f8cd97e65cd50ed24f947f89fffcac364
Max <mpjoles@gmail.com>
2022-03-25 16:10:03 -0400

    auto

--- div-hunter.js
+++ div-hunter.js
@@ -7,14 +7,23 @@ const styleSheets = document.styleSheets;
 for (const key in styleSheets) {
     if (styleSheets.hasOwnProperty.call(styleSheets, key)) {
         let styleSheet = styleSheets[key];
+        let styeSheetUrl = new URL (styleSheet.href);
+        let styleSheetDomain = styeSheetUrl.hostname;
+
+        if (hostname == "assets-global.website-files.com" || hostname == "uploads-ssl.webflow.com" ) {
+            console.log("Is Webflow Sitemap")
+            console.log(styeSheetUrl);
+        }
+
+
         console.log(styleSheet);
-        
-        
+     
     }
 }
 
 hunterForm.addEventListener("submit", (e) => {
     let siteLink = hunterLink.ariaValueMax;
     console.log(siteLink);
+    console.log("Form Submitted");
 
 } )
